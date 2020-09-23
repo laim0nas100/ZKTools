474234f887d7cc5135edd0d845ab51d7e6cb955a
laim0nas100 <aciukadsiunciate@gmail.com>
2020-09-23 14:19:58 +0300

    update

--- src/main/java/lt/lb/zk/rows/builder/DefaultZKDrowsBuilder.java
+++ src/main/java/lt/lb/zk/rows/builder/DefaultZKDrowsBuilder.java
@@ -11,27 +11,26 @@ import lt.lb.zk.builder.CTX;
  *
  * @author laim0nas100
  */
-public abstract class DefaultZKDrowsBuilder<R extends ZKBaseDrow<R,RR>, RR extends ZKBaseDrows<R,RR>,C extends CTX> implements ZKRowsBuilder<C,RR> {
+public abstract class DefaultZKDrowsBuilder<R extends ZKBaseDrow<R, RR>, RR extends ZKBaseDrows<R, RR>, C extends CTX> implements ZKRowsBuilder<C, RR> {
 
     protected Component root;
     protected RR rows;
     protected ZKBaseDrowConf<R, RR> rowConf;
     protected ZKBaseDrowsConfig<R, RR> rowsConf;
-    
+
     protected C context;
 
     @Override
     public abstract RR build(Component root);
 
     protected abstract C produceContext();
-    
+
     @Override
     public C getContext() {
-        if(context == null){
+        if (context == null) {
             context = produceContext();
         }
         return context;
     }
-    
 
 }
