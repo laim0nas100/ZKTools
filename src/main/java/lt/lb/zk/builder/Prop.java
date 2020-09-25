6b9c899ea1e2a7c3505e4308466e4fbf0425eefd
laim0nas100 <aciukadsiunciate@gmail.com>
2020-09-25 15:00:34 +0300

    update

--- src/main/java/lt/lb/zk/builder/Prop.java
+++ src/main/java/lt/lb/zk/builder/Prop.java
@@ -16,61 +16,105 @@ public class Prop<T> extends Props.PropGet<String> {
     public boolean optional;
     public Props props;
 
-    public T getDefault(){
+    public T getDefault() {
         return defaultVal;
     }
-    
-    public void set(Object obj){
+
+    public void set(Object obj) {
         String string = obj == null ? null : String.valueOf(obj);
         insert(props, string);
     }
-    
-    public boolean isOptional(){
+
+    public boolean isOptional() {
         return optional;
     }
-    
-    public SafeOpt<Prop<T>> getSafe(){
+
+    public SafeOpt<Prop<T>> getSafe() {
         return SafeOpt.of(this);
     }
-    
+
     public boolean getBool() {
         return Boolean.parseBoolean(get(props));
+    }
+
+    public SafeOpt<Boolean> getSafeBool() {
+        return getSafe().map(m -> m.getBool());
     }
 
     public int getInt() {
         return Integer.parseInt(get(props));
     }
 
+    public SafeOpt<Integer> getSafeInt() {
+        return getSafe().map(m -> m.getInt());
+    }
+
     public long getLong() {
         return Long.parseLong(get(props));
+    }
+
+    public SafeOpt<Long> getSafeLong() {
+        return getSafe().map(m -> m.getLong());
     }
 
     public float getFloat() {
         return Float.parseFloat(get(props));
     }
 
+    public SafeOpt<Float> getSafeFloat() {
+        return getSafe().map(m -> m.getFloat());
+    }
+
     public double getDouble() {
         return Double.parseDouble(get(props));
+    }
+
+    public SafeOpt<Double> getSafeDouble() {
+        return getSafe().map(m -> m.getDouble());
     }
 
     public List<Integer> getIntList() {
         return Stream.of(parseArray(get(props))).map(s -> Integer.parseInt(s)).collect(Collectors.toList());
     }
 
+    public SafeOpt<List<Integer>> getSafeIntList() {
+        return getSafe().map(m -> m.getIntList());
+    }
+
     public List<Long> getLongList() {
         return Stream.of(parseArray(get(props))).map(s -> Long.parseLong(s)).collect(Collectors.toList());
+    }
+
+    public SafeOpt<List<Long>> getSafeLongList() {
+        return getSafe().map(m -> m.getLongList());
     }
 
     public List<Float> getFloatList() {
         return Stream.of(parseArray(get(props))).map(s -> Float.parseFloat(s)).collect(Collectors.toList());
     }
 
+    public SafeOpt<List<Float>> getSafeFloatList() {
+        return getSafe().map(m -> m.getFloatList());
+    }
+
     public List<Double> getDoubleList() {
         return Stream.of(parseArray(get(props))).map(s -> Double.parseDouble(s)).collect(Collectors.toList());
     }
 
+    public SafeOpt<List<Double>> getSafeDoubleList() {
+        return getSafe().map(m -> m.getDoubleList());
+    }
+
     private String[] parseArray(String string) {
         return string.replace("[", "").replace("]", "").split(", ");
+    }
+
+    public List<String> getList() {
+        return Stream.of(parseArray(get(props))).collect(Collectors.toList());
+    }
+
+    public SafeOpt<List<String>> getSafeList() {
+        return getSafe().map(m -> m.getList());
     }
 
     public Prop(String propKey) {
